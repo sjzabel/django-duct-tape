e69804833c5f68b6fd05a588b9c948b641f59114
Stephen Zabel <szabel@q2software.com>
2011-12-13 10:58:51 -0600

    Figuring out setup.py

--- duct_tape/__init__.py
+++ duct_tape/__init__.py
@@ -1,8 +1,8 @@
-from piston.emitters import Emitter
-from duct_tape.api.emitters import CSVEmitter, ExtJSONEmitter
-from piston.emitters import JSONEmitter
-
-Emitter.register('csv', CSVEmitter, 'text/csv; charset=utf-8')
-#Emitter.register('json', JSONEmitter, 'application/json; charset=utf-8')
-Emitter.register('ext-json', ExtJSONEmitter, 'application/json; charset=utf-8')
+#from piston.emitters import Emitter
+#from duct_tape.api.emitters import CSVEmitter, ExtJSONEmitter
+#from piston.emitters import JSONEmitter
+#
+#Emitter.register('csv', CSVEmitter, 'text/csv; charset=utf-8')
+##Emitter.register('json', JSONEmitter, 'application/json; charset=utf-8')
+#Emitter.register('ext-json', ExtJSONEmitter, 'application/json; charset=utf-8')
 
